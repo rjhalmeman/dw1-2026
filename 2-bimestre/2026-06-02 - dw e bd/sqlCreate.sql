c251fb6d51dc236442f2db3f4aea3d46f7a5eb33
Radames <rjhalmeman@gmail.com>
2026-06-02 11:48:17 -0300

    02/06/2026 - 11:48:17

--- 2-bimestre/2026-06-02 - dw e bd/sqlCreate.sql
+++ 2-bimestre/2026-06-02 - dw e bd/sqlCreate.sql
@@ -2,7 +2,7 @@ no postgresql
 
 Crie um banco de dados chamado dw1-db-2026
 
-Neste banco de dados, crie uma tabela chamada pessoa.
+Neste banco de dados, crie uma tabela chamada pessoa (em public).
 
 CREATE TABLE public.pessoa (
 	cpf_pessoa varchar(15) NOT NULL,
