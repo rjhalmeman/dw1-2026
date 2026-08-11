a51efb1187458e74c3c277453f09b75c92ebe4d5
Radames <rjhalmeman@gmail.com>
2026-08-11 11:45:41 -0300

    11/08/2026 - 11:45:41

---
+++ 3-bimestre/2026-08-11-CrudCarro/Carro.js
@@ -0,0 +1,10 @@
+class Carro {
+    constructor (id_carro, nome_carro, modelo_carro, ano, posicaoNaLista) {
+        this.id_carro = id_carro;
+        this.nome_carro = nome_carro;
+        this.modelo_carro = modelo_carro;
+        this.ano = ano;
+
+        this.posicaoNaLista = posicaoNaLista; //atributo para facilitar a alteração e exclusão 
+    }
+}
